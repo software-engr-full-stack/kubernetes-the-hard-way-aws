9b6c45d22b9ec482c8d0120f0bdc7870b32a4f10
michael <software.engr.full.stack@gmail.com>
2022-03-23 19:17:01 -0700

    Show expected output for verification

--- sections/09-bootstrapping-the-kubernetes-worker-nodes/03-verification-using-controller.sh
+++ sections/09-bootstrapping-the-kubernetes-worker-nodes/03-verification-using-controller.sh
@@ -6,6 +6,13 @@ run() {
   local caller="$(basename --suffix .sh "$BASH_SOURCE" | cut -f 4 -d '-')"
 
   CALLER="$caller" "$this_dir/../../lib/ssh.sh" kubectl get nodes --kubeconfig admin.kubeconfig
+
+  echo
+  echo '... expected output (depending on number of workers)...'
+  cat <<EOF
+NAME             STATUS   ROLES    AGE     VERSION
+ip-10-240-0-20   Ready    <none>   5m49s   v1.21.0
+EOF
 }
 
 set -o errexit
