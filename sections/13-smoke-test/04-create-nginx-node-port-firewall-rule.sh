1c1e5a06f0d543a16b14535eb5d10e00fd2c4590
michael <software.engr.full.stack@gmail.com>
2022-03-25 18:12:13 -0700

    Exit if no node port is found

--- sections/13-smoke-test/04-create-nginx-node-port-firewall-rule.sh
+++ sections/13-smoke-test/04-create-nginx-node-port-firewall-rule.sh
@@ -6,6 +6,8 @@ run() {
   local NODE_PORT=$(kubectl get svc nginx \
     --output=jsonpath='{range .spec.ports[0]}{.nodePort}')
 
+  [ -n "$NODE_PORT" ] || exit 1
+
   cd "$this_dir"/..
 
   terraform apply -auto-approve -var=nginx_kubernetes_node_port="$NODE_PORT"
