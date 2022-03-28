c26bc7d287ab7c3e2c53a8249c68f52306e4700f
michael <software.engr.full.stack@gmail.com>
2022-03-27 18:28:48 -0700

    Add config handler

---
+++ lib/config.py
@@ -0,0 +1,18 @@
+import yaml
+
+
+class Config(object):
+    def __init__(self, config_file):
+        with open(config_file, 'r') as stream:
+            try:
+                data = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                raise yaml.YAMLError('YAML error: {}'.format(exc))
+
+        self.organization = data['organization']
+        self.controllers = data['controllers']
+        self.workers = data['workers']
+
+        self.all_hostnames = [
+            host['hostname'] for host in [*self.controllers, *self.workers]
+        ]
