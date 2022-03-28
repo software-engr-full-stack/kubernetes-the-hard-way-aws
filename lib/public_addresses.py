eae00a1ff64c85ec0cc41ebe7e3e75760664c194
michael <software.engr.full.stack@gmail.com>
2022-03-27 19:43:17 -0700

    Include main public IP to public addresses

--- lib/public_addresses.py
+++ lib/public_addresses.py
@@ -6,7 +6,7 @@ import json
 
 
 class PublicAddresses(object):
-    def __init__(self, hostnames):
+    def __init__(self, name, hostnames):
         if len(hostnames) < 1:
             raise ValueError('... ERROR: must pass at least 1 hostname')
 
@@ -32,9 +32,28 @@ class PublicAddresses(object):
                     )
                 )
 
-        self.__data = data
+        result = subprocess.run([
+            'aws', 'ec2', 'describe-addresses',
+            '--filter',
+            'Name=tag:Name,Values={}'.format(name),
+            '--query', 'Addresses[0].PublicIp',
+            '--output', 'text'
+        ], stdout=subprocess.PIPE)
+
+        public_ip = result.stdout.decode('utf-8').strip()
+        if public_ip.strip() == '' or public_ip == 'None':
+            raise ValueError("... ERROR: public IP for '{}' not found".format(name))
+
+        self.__data = {**data, 'kubernetes': public_ip}
 
     def __getitem__(self, key):
+        if key not in self.__data:
+            raise ValueError(
+                "... ERROR: given hostname '{}' not in provisioned hosts table...\n{}".format(
+                    key,
+                    self.__data
+                )
+            )
         return self.__data[key]
 
 
