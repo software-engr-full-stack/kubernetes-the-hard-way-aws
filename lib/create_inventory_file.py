5ff2578892a665a6a6ce513c7f4167340920a801
michael <software.engr.full.stack@gmail.com>
2022-03-30 00:14:04 -0700

    Update to fit interface with rest of the code

--- lib/create_inventory_file.py
+++ lib/create_inventory_file.py
@@ -1,72 +1,42 @@
 #!/usr/bin/env python3
 
 import argparse
-import sys
 import pathlib
-
-from config import Config
-from public_addresses import PublicAddresses
+import re
 
 
 class CreateInventoryFile(object):
-    def __init__(self):
-        parser = argparse.ArgumentParser(description='Create inventory file')
-
-        parser.add_argument(
-            '-c', '--config-file',
-            dest='config_file',
-            required=True,
-            help='the config file'
-        )
-
-        parser.add_argument(
-            '-t', '--host-type',
-            dest='host_type',
-            required=True,
-            help='host type whether "controller" or "worker"'
-        )
-
-        parser.add_argument(
-            '-o', '--inventory-file',
-            dest='inventory_file',
-            required=True,
-            help='the output file'
-        )
-
-        args = parser.parse_args()
-
-        config = Config(args.config_file)
-
-        public_addresses = PublicAddresses(config.all_hostnames)
-
-        host_type_table = {
-            'controller': config.controller_hostnames,
-            'worker': config.worker_hostnames
-        }
-        if args.host_type not in host_type_table:
-            raise ValueError(
-                "... ERROR: invalid host type '{}', valid host types...\n{}".format(
-                    args.host_type, host_type_table
-                )
-            )
-
-        ips = [public_addresses[hname] for hname in host_type_table[args.host_type]]
-
+    def __init__(self, ip_addresses, inventory_file):
         this_dir = pathlib.Path(__file__).parent.resolve()
         template_file = this_dir.joinpath('inventory.template')
 
-        inventory_file = pathlib.Path(args.inventory_file).resolve()
+        inventory_file = pathlib.Path(inventory_file).resolve()
         inventory_file.parent.mkdir(parents=True, exist_ok=True)
 
         with open(template_file, 'r') as fh:
             template_str = fh.read()
 
         with open(inventory_file, 'w') as fh:
-            fh.write(template_str.format(remote_ips="\n".join(ips)))
+            fh.write(template_str.format(remote_ips="\n".join(ip_addresses)))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        raise ValueError('... ERROR: must pass remote IPs')
+    parser = argparse.ArgumentParser(description='Create inventory file')
 
-    CreateInventoryFile()
+    parser.add_argument(
+        '-i', '--ip-addresses',
+        dest='ip_addresses',
+        required=True,
+        help='IP addresses separated by space'
+    )
+
+    parser.add_argument(
+        '-o', '--inventory-file',
+        dest='inventory_file',
+        required=True,
+        help='the output file'
+    )
+
+    args = parser.parse_args()
+
+    CreateInventoryFile(re.split(r'\s+', args.ip_addresses), args.inventory_file)
