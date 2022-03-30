5236fb922fb977f10904f7f1705e538be437c92b
michael <software.engr.full.stack@gmail.com>
2022-03-29 19:02:06 -0700

    Convert to Python: 06-generating-the-data-encryption-config-and-key

---
+++ sections/06-generating-the-data-encryption-config-and-key/run.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import sys
+import pathlib
+import os
+import inspect
+from cryptography.fernet import Fernet
+
+this_file = pathlib.Path((inspect.getfile(inspect.currentframe())))
+app_dir = this_file.joinpath('..', '..', '..').resolve()
+sys.path.insert(0, app_dir.as_posix())
+
+from lib.path import Path  # noqa: E402
+
+
+class Run(object):
+    def __init__(self):
+        encryption_key = Fernet.generate_key().decode('utf-8')
+        path = Path()
+
+        config_auto_gen_path = path.config_auto_gen
+        config_auto_gen_path.mkdir(parents=True, exist_ok=True)
+
+        os.chdir(config_auto_gen_path)
+        out_file = config_auto_gen_path.joinpath('encryption-config.yaml')
+        with open(out_file, 'w') as stream:
+            stream.write(inspect.cleandoc(
+                '''
+                    kind: EncryptionConfig
+                    apiVersion: v1
+                    resources:
+                      - resources:
+                          - secrets
+                        providers:
+                          - aescbc:
+                              keys:
+                                - name: key1
+                                  secret: {}
+                          - identity: {{}}
+                '''.format(encryption_key)
+            ))
+
+
+Run()
