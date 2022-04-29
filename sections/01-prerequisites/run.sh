2a62094f09e3ecb43f8ffe4ae8e19aafacac443c
michael <software.engr.full.stack@gmail.com>
2022-04-29 16:37:38 -0700

    Automate the checking of prerequisites

--- sections/01-prerequisites/run.sh
+++ sections/01-prerequisites/run.sh
@@ -1,10 +1,37 @@
 #!/usr/bin/env bash
 
 run() {
-  if ! type aws >/dev/null; then
-    echo 'ERROR: must install AWS CLI tool' >&2
-    exit 1
-  fi
+  declare -a required_tools=(
+    aws
+    kubectl
+    terraform
+    pip
+    cfssl
+    cfssljson
+  )
+  local cmd=
+  for cmd in "${required_tools[@]}"; do
+    if ! type "$cmd" >/dev/null; then
+      echo "ERROR: must install '$cmd'" >&2
+      exit 1
+    fi
+  done
+
+  declare -a required_python_packages=(
+    ansible
+    pyyaml:yaml
+    cryptography
+  )
+
+  local pkg_import=
+  local pkg=
+  local import=
+  for pkg_import in "${required_python_packages[@]}"; do
+    pkg="$(cut --delimiter ':' --field 1 <<<"$pkg_import")"
+    import="$(cut --delimiter ':' --only-delimited --field 2 <<<"$pkg_import")"
+    [ -n "$import" ] || import="$pkg"
+    python -c "import $import"
+  done
 
   if ! AWS_PAGER='' aws sts get-caller-identity >/dev/null; then
     echo 'ERROR: must setup AWS credentials' >&2
